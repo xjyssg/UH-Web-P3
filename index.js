29ba4a88f7d6ea40be1f50db1e775cda05107341
xjyssg <jiayuexue30@gmail.com>
2020-09-17 19:32:38 +0800

    3.4

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+
 app.get('/info', (req, res) => {
   var myDate = new Date();
   res.send(`
